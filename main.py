f0e87499f1cdfee42612bc0b17d693328fe004a6
shirowanisan <shirowani333@gmail.com>
2023-01-03 00:03:47 +0900

    Fix main

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import os
 
 import soundfile as sf
 
-from coeirocore.coeiroink_manager import EspnetModel
+from coeirocore.coeiro_manager import AudioManager
 
 if __name__ == '__main__':
     output_dir = 'output'
     os.makedirs(output_dir, exist_ok=True)
-    wav = EspnetModel.get_character_model(style_id=1).make_voice('こんにちは')
+    wav = AudioManager().synthesis('こんにちは', style_id=1)
     sf.write(f"{output_dir}/output.wav", wav, 44100, 'PCM_16')
