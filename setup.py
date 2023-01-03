5af10d0711a71e1736fb86e1e9d8438e171a6c70
shirowanisan <shirowani333@gmail.com>
2023-01-03 15:45:39 +0900

    Update to v.1.0.0

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ import setuptools
 
 setuptools.setup(
     name='coeirocore',
-    version='0.0.1',
+    version='1.0.0',
     py_modules=['coeirocore.*', 'coeirocore/coeiro_manager', 'coeirocore/query_manager']
 )
