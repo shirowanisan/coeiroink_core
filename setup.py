fd950ecf09678a21946ab4578e77587bda36d3cb
shirowanisan <shirowani333@gmail.com>
2023-01-15 14:54:46 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ import setuptools
 
 setuptools.setup(
     name='coeirocore',
-    version='1.0.1',
+    version='1.0.2',
+    url="https://github.com/shirowanisan/coeiroink_core",
+    author="shirowanisan",
+    license="LGPL license",
     py_modules=['coeirocore.*', 'coeirocore/coeiro_manager', 'coeirocore/query_manager']
 )
